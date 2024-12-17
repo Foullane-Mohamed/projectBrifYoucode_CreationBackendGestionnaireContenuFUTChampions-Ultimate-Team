56f8f5ed55be7d6de421d5f0b95b42d359a34aa5
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2024-12-17 16:58:46 +0100

    ajouter dashbord

---
+++ src/logique.js
@@ -0,0 +1,47 @@
+
+let selectPosition = document.querySelector(".select-position");
+let inputsDivplayerGk = document.querySelector(".player-gk");
+let inputsDivplayerCm = document.querySelector(".player-cm");
+let showForm = document.querySelector(".showForm");
+let fromData = document.querySelector(".fromData");
+let btnEnter = document.querySelector(".btn-enter");
+let card = document.querySelector(".card");
+let showInfoPlayer = document.querySelector(".show-info-player");
+let hideInfoPlayer = document.querySelector(".hide-info-player");
+selectPosition.addEventListener("change", () => {
+if (selectPosition.value === "GK") {
+inputsDivplayerGk.style.display = "block";
+inputsDivplayerCm.style.display = "none";
+} else {
+inputsDivplayerGk.style.display = "none";
+inputsDivplayerCm.style.display = "block";
+}
+});
+
+showForm.addEventListener("click", () => {
+
+  fromData.style.display = "block";
+  card.style.display = "none";
+
+});
+btnEnter.addEventListener("click", () => {
+
+  fromData.style.display = "none";
+  
+});
+showInfoPlayer.addEventListener("click", () => {
+  
+  card.style.display = "block";
+
+  
+
+});
+hideInfoPlayer.addEventListener("click", () => {
+  
+  card.style.display = "none";
+  
+  
+
+});
+
+
